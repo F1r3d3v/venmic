2223a707f800773341a0ffcbef8ae02da5dc229a
F1r3d3v <ogieniewski.dev@gmail.com>
2025-07-01 21:40:02 +0200

    Adds build scripts and fixes dependency
    Adds build scripts for addon and server components.
    These scripts streamline the build process and ensure consistent builds.
    
    Includes a patch to fix rohrkabel dependency by adding variant include.
    Adds .cpm-cache to .gitignore.

---
+++ scripts/build_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+readonly SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+readonly CPM_CACHE_DIR="${SCRIPT_DIR}/../.cpm-cache"
+
+# Export Threads for Make and Ninja
+export MAKEFLAGS=-j$(nproc)
+export PARALLEL_LEVEL=$(nproc)
+export CPM_SOURCE_CACHE="${CPM_CACHE_DIR}"
+
+cmake -B build && cmake --build build
+
