1b68c3d12537e0d2f7e71104cab02649c77cbf08
F1r3d3v <ogieniewski.dev@gmail.com>
2025-07-02 01:09:31 +0200

    Fixes build process for venmic addon

--- scripts/build_addon.sh
+++ scripts/build_addon.sh
@@ -3,10 +3,7 @@
 readonly SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 readonly CPM_CACHE_DIR="${SCRIPT_DIR}/../.cpm-cache"
 
-# Export Threads for Make and Ninja
-export MAKEFLAGS=-j$(nproc)
-export PARALLEL_LEVEL=$(nproc)
 export CPM_SOURCE_CACHE="${CPM_CACHE_DIR}"
 
-pnpm install
+pnpm install && pnpm run build
 
